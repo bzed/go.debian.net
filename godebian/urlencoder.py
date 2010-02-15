798543db7ae43cf27a06f00301f2821acef8f274
Bernd Zeimetz <bernd@bzed.de>
2010-02-15 12:19:14 +0100

    Add MIT license and copyrights to urlencoder.py

--- godebian/urlencoder.py
+++ godebian/urlencoder.py
@@ -1,6 +1,33 @@
-# Python Short URL Generator
-# Based on http://code.activestate.com/recipes/576918/
-# by Michael Fogleman
+# -*- coding: utf-8 -*-
+__author__ = "Michael Fogleman, Bernd Zeimetz"
+__contact__ = "bzed@debian.org"
+__credits__ = ['http://www.michaelfogleman.com/2009/09/python-short-url-generator/']
+__license__ = """
+Copyright (C) 2009 Michael Fogleman
+Copyright (C) 2010 Bernd Zeimetz <bzed@debian.org>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from .config import UrlencoderConfig
 DEFAULT_ALPHABET = UrlencoderConfig.alphabet
 DEFAULT_BLOCK_SIZE = UrlencoderConfig.blocksize
