2691542a3732570b09936ff1e2e8e9fd600a14d1
Bernd Zeimetz <bernd@bzed.de>
2010-05-08 15:37:18 +0200

    Replace my own memcache implementation by one from werkzeug.

--- godebian/cache.py
+++ godebian/cache.py
@@ -29,7 +29,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import hashlib
-
+from  werkzeug.contrib.cache import MemcachedCache
 from .config import MemcachedConfig
 
 try:
@@ -37,37 +37,11 @@ try:
 except ImportError:
     import memcache
 
-class MemCacheClass(object):
-    def __init__(self, servers, timeout, prefix):
-        self._client = memcache.Client(servers)
-        self._timeout = timeout
-        self._prefix = prefix
-
-    def _pkey(self, key):
-        """ Add prefix to the key and return the md5 hexdigest value of it.
-        This allowed to use types != str as key - memcached is able to
-        handle str only.
-        """
-        m = hashlib.md5()
-        m.update("%s:%s" %(self._prefix, key))
-        return str(m.hexdigest())
-
-    def add(self, key, value):
-        return self._client.add(self._pkey(key), value, self._timeout)
-
-    def set(self, key, value):
-        return self._client.set(self._pkey(key), value, self._timeout)
-
-    def get(self, key, default=None):
-        value = self._client.get(self._pkey(key))
-        if value is None:
-            return default
-        return value
-
-    def close(self):
-        self._client.disconnect_all()
-
-_servers = MemcachedConfig.servers.split(';')
+_client = memcache.Client(MemcachedConfig.servers.split(';'))
 _timeout = MemcachedConfig.timeout
 _prefix = MemcachedConfig.prefix
-MemCache = MemCacheClass(_servers, _timeout, _prefix)
+MemCache = MemcachedCache(_client,
+                          default_timeout=_timeout,
+                          key_prefix=_prefix)
+__all__ = ["MemCache"]
+
